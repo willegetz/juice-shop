675f8db746b1b7eeff327ba8c9505054a7f435a1
William Getz <willegetz@gmail.com>
2019-02-12 13:08:42 -0800

    Updated to use insecureew.js

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,29 +1,33 @@
-const utils = require('../lib/utils')
-const insecurity = require('../lib/insecurity')
-const challenges = require('../data/datacache').challenges
+'use strict';
+
+const utils = require('../lib/utils');
+const challenges = require('../data/datacache').challenges;
+
+const container = require('../container');
+const insecurity = container.build('insecurityNew');
 
 module.exports = function performRedirect () {
   return ({ query }, res, next) => {
-    const toUrl = query.to
+    const toUrl = query.to;
     if (insecurity.isRedirectAllowed(toUrl)) {
       if (utils.notSolved(challenges.redirectGratipayChallenge) && toUrl === 'https://gratipay.com/juice-shop') {
-        utils.solve(challenges.redirectGratipayChallenge)
+        utils.solve(challenges.redirectGratipayChallenge);
       }
       if (utils.notSolved(challenges.redirectChallenge) && isUnintendedRedirect(toUrl)) {
-        utils.solve(challenges.redirectChallenge)
+        utils.solve(challenges.redirectChallenge);
       }
-      res.redirect(toUrl)
+      res.redirect(toUrl);
     } else {
-      res.status(406)
-      next(new Error('Unrecognized target URL for redirect: ' + toUrl))
+      res.status(406);
+      next(new Error('Unrecognized target URL for redirect: ' + toUrl));
     }
-  }
-}
+  };
+};
 
 function isUnintendedRedirect (toUrl) {
-  let unintended = true
+  let unintended = true;
   for (let allowedUrl of insecurity.redirectWhitelist) {
-    unintended = unintended && !utils.startsWith(toUrl, allowedUrl)
+    unintended = unintended && !utils.startsWith(toUrl, allowedUrl);
   }
-  return unintended
+  return unintended;
 }
