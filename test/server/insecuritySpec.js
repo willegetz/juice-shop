3e3a3fd45191276db6661cef228c5bad32174001
William Getz <willegetz@gmail.com>
2019-02-11 18:56:18 -0800

    Added semicolons and extracted for clarity

--- test/server/insecuritySpec.js
+++ test/server/insecuritySpec.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
-const chai = require('chai')
+const chai = require('chai');
 const container = require('../../container');
-const expect = chai.expect
+const expect = chai.expect;
 
 describe('insecurity', () => {
   let insecurity;
@@ -17,10 +17,10 @@ describe('insecurity', () => {
     it('returns string unchanged if it contains no null byte', function () {
       const fileName = 'file.exe.pdf';
 
-      const cleansedFileName = insecurity.cutOffPoisonNullByte(fileName);
+      const actualCleansedFileName = insecurity.cutOffPoisonNullByte(fileName);
 
-      return expect(cleansedFileName).to.equal(fileName);
-    })
+      return expect(actualCleansedFileName).to.equal(fileName);
+    });
 
     it('returns string up to null byte', function () {
       const fileNameWithNullByte = 'file.exe%00.pdf';
@@ -29,22 +29,35 @@ describe('insecurity', () => {
       const actualCleansedFileName = insecurity.cutOffPoisonNullByte(fileNameWithNullByte);
 
       return expect(actualCleansedFileName).to.equal(excpectedCleansedFileName);
-    })
-  })
+    });
+  });
 
   describe('userEmailFrom', () => {
     it('returns content of "x-user-email" header if present', () => {
-      expect(insecurity.userEmailFrom({ headers: { 'x-user-email': 'test@bla.blubb' } })).to.equal('test@bla.blubb')
-    })
+      const headers = { headers: { 'x-user-email': 'test@bla.blubb' } };
+
+      const actualEmail = insecurity.userEmailFrom(headers);
+      const expectedEmail = 'test@bla.blubb';
+
+      expect(actualEmail).to.equal(expectedEmail);
+    });
 
     it('returns undefined if header "x-user-email" is not present', () => {
-      expect(insecurity.userEmailFrom({ headers: {} })).to.equal(undefined)
-      expect(insecurity.userEmailFrom({})).to.equal(undefined)
-    })
-  })
+      const headersWithoutContent = { headers: {} };
+      const emptyObjectForHeaders = {};
+
+      const undefinedEmailFromHeadersWithoutContent = insecurity.userEmailFrom(headersWithoutContent);
+      const undefinedEmailFromEmptyHeadersObject = insecurity.userEmailFrom(emptyObjectForHeaders);
+
+      const expectedUndefinedEmail = undefined;
+
+      expect(undefinedEmailFromHeadersWithoutContent).to.equal(expectedUndefinedEmail);
+      expect(undefinedEmailFromEmptyHeadersObject).to.equal(expectedUndefinedEmail);
+    });
+  });
 
   describe('generateCoupon', () => {
-    const z85 = require('z85')
+    const z85 = require('z85');
 
     it('returns base85-encoded month, year and discount as coupon code', () => {
       const couponDate = new Date('1980-01-01T00:00:00');
@@ -57,52 +70,78 @@ describe('insecurity', () => {
 
       expect(actualCouponEncoded).to.equal(expectedCouponEncoded);
       expect(actualCouponDecoded.toString()).to.equal(expectedCouponDecoded);
-    })
+    });
 
     it('uses current month and year if not specified', () => {
-      const coupon = insecurity.generateCoupon(20)
-      expect(coupon).to.equal(insecurity.generateCoupon(20, new Date()))
-    })
+      const coupon20Percent = insecurity.generateCoupon(20);
+
+      const expected20PercentCoupon = insecurity.generateCoupon(20, new Date());
+
+      expect(coupon20Percent).to.equal(expected20PercentCoupon);
+    });
 
     it('does not encode day of month or time into coupon code', () => {
-      const coupon = insecurity.generateCoupon(10, new Date('December 01, 1999'))
-      expect(coupon).to.equal(insecurity.generateCoupon(10, new Date('December 01, 1999 01:00:00')))
-      expect(coupon).to.equal(insecurity.generateCoupon(10, new Date('December 02, 1999')))
-      expect(coupon).to.equal(insecurity.generateCoupon(10, new Date('December 31, 1999 23:59:59')))
-    })
-  })
+      const coupon = insecurity.generateCoupon(10, new Date('December 01, 1999'));
+
+      const december01WithTimestamp = insecurity.generateCoupon(10, new Date('December 01, 1999 01:00:00'));
+      const december02NoTimestamp = insecurity.generateCoupon(10, new Date('December 02, 1999'));
+      const december31WithTimestamp = insecurity.generateCoupon(10, new Date('December 31, 1999 23:59:59'));
+
+      expect(coupon).to.equal(december01WithTimestamp);
+      expect(coupon).to.equal(december02NoTimestamp);
+      expect(coupon).to.equal(december31WithTimestamp);
+    });
+  });
 
   describe('discountFromCoupon', () => {
-    const z85 = require('z85')
+    const z85 = require('z85');
 
     it('returns undefined when not passing in a coupon code', () => {
-      expect(insecurity.discountFromCoupon(undefined)).to.equal(undefined)
-      expect(insecurity.discountFromCoupon(null)).to.equal(undefined)
+      const undefinedCouponCode = insecurity.discountFromCoupon(undefined);
+      const nullCouponCode = insecurity.discountFromCoupon(null);
+
+      expect(undefinedCouponCode).to.equal(undefined);
+      expect(nullCouponCode).to.equal(undefined);
     })
 
     it('returns undefined for malformed coupon code', () => {
-      expect(insecurity.discountFromCoupon('')).to.equal(undefined)
-      expect(insecurity.discountFromCoupon('x')).to.equal(undefined)
-      expect(insecurity.discountFromCoupon('___')).to.equal(undefined)
-    })
+      const emptyStringCouponCode = insecurity.discountFromCoupon('');
+      const letterXCouponCode = insecurity.discountFromCoupon('x');
+      const underscoresCouponCode = insecurity.discountFromCoupon('___');
+
+      expect(emptyStringCouponCode).to.equal(undefined);
+      expect(letterXCouponCode).to.equal(undefined);
+      expect(underscoresCouponCode).to.equal(undefined);
+    });
 
     it('returns undefined for coupon code not according to expected pattern', () => {
-      expect(insecurity.discountFromCoupon(z85.encode('Test'))).to.equal(undefined)
-      expect(insecurity.discountFromCoupon(z85.encode('XXX00-10'))).to.equal(undefined)
-      expect(insecurity.discountFromCoupon(z85.encode('DEC18-999'))).to.equal(undefined)
-      expect(insecurity.discountFromCoupon(z85.encode('DEC18-1'))).to.equal(undefined)
-      expect(insecurity.discountFromCoupon(z85.encode('DEC2018-10'))).to.equal(undefined)
-    })
+      const wordForCouponCode = insecurity.discountFromCoupon(z85.encode('Test'));
+      const invalidMonthAndYear = insecurity.discountFromCoupon(z85.encode('XXX00-10'));
+      const tooManyNumbersDiscountPercent = insecurity.discountFromCoupon(z85.encode('DEC18-999'));
+      const notEnoughNumbersDiscountPercent = insecurity.discountFromCoupon(z85.encode('DEC18-1'));
+      const fourDigitsForYear = insecurity.discountFromCoupon(z85.encode('DEC2018-10'));
+
+      expect(wordForCouponCode).to.equal(undefined);
+      expect(invalidMonthAndYear).to.equal(undefined);
+      expect(tooManyNumbersDiscountPercent).to.equal(undefined);
+      expect(notEnoughNumbersDiscountPercent).to.equal(undefined);
+      expect(fourDigitsForYear).to.equal(undefined);
+    });
 
     it('returns undefined for expired coupon code', () => {
-      expect(insecurity.discountFromCoupon(z85.encode('SEP14-50'))).to.equal(undefined)
-    })
+      const expiredCoupon = insecurity.discountFromCoupon(z85.encode('SEP14-50'));
+
+      expect(expiredCoupon).to.equal(undefined);
+    });
 
     it('returns discount from valid coupon code', () => {
-      expect(insecurity.discountFromCoupon(insecurity.generateCoupon(5))).to.equal(5)
+      const valid5Percent = insecurity.discountFromCoupon(insecurity.generateCoupon(5));
+      const valid10Percent = insecurity.discountFromCoupon(insecurity.generateCoupon(10));
+      const valid99Percent = insecurity.discountFromCoupon(insecurity.generateCoupon(99));
 
-      expect(insecurity.discountFromCoupon(insecurity.generateCoupon(10))).to.equal(10)
-      expect(insecurity.discountFromCoupon(insecurity.generateCoupon(99))).to.equal(99)
-    })
-  })
-})
+      expect(valid5Percent).to.equal(5);
+      expect(valid10Percent).to.equal(10);
+      expect(valid99Percent).to.equal(99);
+    });
+  });
+});
